8f4016fe6968cc1f572102c323b3df011b71fee3
Claire Ongley <claireongley@hotmail.com>
2022-05-06 20:18:02 +1000

    starting new project

---
+++ iosGroup assignment/iosGroup assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosGroup assignment
+//
+//  Created by Claire Ongley on 6/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
