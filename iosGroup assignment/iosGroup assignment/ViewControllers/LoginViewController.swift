f94aba0932fe107e4469ba3eb9798ba4cd07870b
Claire Ongley <claireongley@hotmail.com>
2022-05-18 21:50:37 +1000

    log tabs created and login screen

---
+++ iosGroup assignment/iosGroup assignment/ViewControllers/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  iosGroup assignment
+//
+//  Created by Claire Ongley on 18/5/22.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+}
