099e6050c24f6c7856ba8200b49c16fdb033fd44
Chenglin Zhang <13699424@student.uts.edu.au>
2022-05-19 19:49:10 +1000

    search API connect

--- iosGroup assignment/iosGroup assignment/ApiServices.swift
+++ iosGroup assignment/iosGroup assignment/ApiServices.swift
@@ -12,7 +12,7 @@ class ApiService {
     let topRatedURL = "https://api.themoviedb.org/3/movie/top_rated?api_key=392edc2e9b59a6d9b11f639b3c2a0f26&language=en-US&page=1"
     let trendingURL = "https://api.themoviedb.org/3/trending/all/day?api_key=392edc2e9b59a6d9b11f639b3c2a0f26"
     let upcomingURL = "https://api.themoviedb.org/3/movie/upcoming?api_key=392edc2e9b59a6d9b11f639b3c2a0f26&language=en-US&page=1"
-    let searchURL = "https://api.themoviedb.org/3/search/movie?api_key=392edc2e9b59a6d9b11f639b3c2a0f26&language=en-US&query=\(searchInput)&page=1&include_adult=false"
+    let searchURL = "https://api.themoviedb.org/3/search/movie?api_key=392edc2e9b59a6d9b11f639b3c2a0f26&language=en-US&query=(searchInput)&page=1&include_adult=false"
     let nowPlaying = "https://api.themoviedb.org/3/movie/now_playing?api_key=392edc2e9b59a6d9b11f639b3c2a0f26&language=en-US&page=1"
     
 }
--- iosGroup assignment/iosGroup assignment/ViewControllers/SearchViewController.swift
+++ iosGroup assignment/iosGroup assignment/ViewControllers/SearchViewController.swift
@@ -15,7 +15,55 @@ class SearchViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    var searchValue: String = ""
 
+    @IBAction func SearchValue(_ sender: Any) {
+    }
+    
+    @IBAction func Search(_ sender: Any) {
+        print("search!")
+        var request = URLRequest(url: URL(string: "https://api.themoviedb.org/3/search/movie?api_key=392edc2e9b59a6d9b11f639b3c2a0f26&language=en-US&query=abc&page=1&include_adult=false")!)
+        request.httpMethod = "GET"
+        let session = URLSession.shared
+        let task = session.dataTask(with: request, completionHandler: { data, response, error -> Void in
+            do {
+                let json = try JSONSerialization.jsonObject(with: data!) as! Dictionary<String, AnyObject>
+                let result:NSArray = (json["results"] as! NSArray)
+                var eachDic:NSDictionary
+                for each in result{
+                    eachDic = each as! NSDictionary
+                    print(eachDic["title"] ?? "No title")
+                    print(eachDic["id"] ?? "No id")
+                    print("-----------------")
+                }
+                
+            } catch {
+                print("error")
+            }
+        })
 
+        task.resume()
+    }
+    
+    @IBOutlet weak var tableValue: UITableView!
+    
+    @IBAction func SearchText(_ sender: Any) {
+        print("search!")
+        var request = URLRequest(url: URL(string: "https://api.themoviedb.org/3/search/movie?api_key=392edc2e9b59a6d9b11f639b3c2a0f26&language=en-US&query=abc&page=1&include_adult=false")!)
+        request.httpMethod = "GET"
+        let session = URLSession.shared
+        let task = session.dataTask(with: request, completionHandler: { data, response, error -> Void in
+            print(response!)
+            do {
+                let json = try JSONSerialization.jsonObject(with: data!) as! Dictionary<String, AnyObject>
+                print(json)
+            } catch {
+                print("error")
+            }
+        })
+
+        task.resume()
+    }
+    
 }
 
